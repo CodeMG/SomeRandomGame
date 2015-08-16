04cfeb76928941b1cad518d459b220ac78becfd0
CodeMG <guel_mehmet@hotmail.de>
2015-08-17 01:56:16 +0200

    Added collision in the Dungeons

--- RandomGame/Unit.java
+++ RandomGame/Unit.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.*;
 
 public class Unit extends Drawobject{
     protected boolean moveRight,moveLeft,moveUp,moveDown;
@@ -7,11 +8,11 @@ public class Unit extends Drawobject{
     public Unit(double x,double y,double width,double height,Scene scene){
         super(x,y,width,height,scene);
     }
-    
+
     public void zeichnen(Graphics g,JPanel panel,int verschiebungX,int verschiebungY){
-        
+
     }
-    
+
     public void calculate(){
         System.out.print(x);
         if(moveRight){
@@ -47,28 +48,50 @@ public class Unit extends Drawobject{
             }
         }
     }
-    
+
     public void moveRight(){
         if((!moveLeft)&&(!moveUp)&&(!moveDown)){
-            moveRight = true;
+            if(!collision((int)(x+Gamepanel.getGRIDSIZE()),(int)y)){
+                moveRight = true;
+            }
         }
     }
 
     public void moveLeft(){
         if((!moveRight) && (!moveUp) && (!moveDown)){
-            moveLeft = true;
+            if(!collision((int)(x-Gamepanel.getGRIDSIZE()),(int)y)){
+                moveLeft = true;
+            }
         }
     }
 
     public void moveUp(){
         if((!moveRight) && (!moveLeft) && (!moveDown)){
-            moveUp = true;
+            if(!collision((int)(x),(int)(y-Gamepanel.getGRIDSIZE()))){
+                moveUp = true;
+            }
         }
     }
 
     public void moveDown(){
         if((!moveLeft) && (!moveRight) && (!moveUp)){
-            moveDown = true;
+            if(!collision((int)(x),(int)(y+Gamepanel.getGRIDSIZE()))){
+                moveDown = true;
+            }
         }
+    }
+
+    public boolean collision(int futureX,int futureY){
+        Rectangle rect1 = new Rectangle(futureX,futureY,Gamepanel.getGRIDSIZE(),Gamepanel.getGRIDSIZE());
+        Rectangle rect2;
+        ArrayList<Obstacle> obstacles = scene.getObstacles();
+
+        for(int i = 0; i < obstacles.size();i++){
+            rect2 = new Rectangle((int)(obstacles.get(i).getX()*obstacles.get(i).getWidth()),(int)(obstacles.get(i).getY()*obstacles.get(i).getHeight()),(int)obstacles.get(i).getWidth(),(int)obstacles.get(i).getHeight());
+            if(rect1.intersects(rect2)){
+                return true;
+            }
+        }
+        return false;
     }
 }
